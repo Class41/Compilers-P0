12fd5dc823237acd3cb63601f112d42122d83298
Vasyl Onufriyev <projectclass41@gmail.com>
2019-08-28 15:51:45 -0500

    added makefile

--- src/com/umsl/vasylonufriyev/Main.java
+++ src/com/umsl/vasylonufriyev/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         String[] parsedData = ParseClassifier.Classify(args);
+        System.out.println(parsedData.length);
     }
 }
