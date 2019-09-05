3102773741db9045c49a2aaf6d73254e0776c9df
Vasyl Onufriyev <projectclass41@gmail.com>
2019-09-05 11:44:21 -0500

    Update ParseSourceType.java

--- src/com/umsl/vasylonufriyev/DatasourceParser/ParseSourceType.java
+++ src/com/umsl/vasylonufriyev/DatasourceParser/ParseSourceType.java
@@ -1,3 +1,11 @@
+/*
+Author: Vasyl Onufriyev
+Date: 8.28.2019
+Class: CS4280
+Instructor: Professor Janikow
+Description: Enum to assist readability of input type when parsing data
+*/
+
 package com.umsl.vasylonufriyev.DatasourceParser;
 
 public enum ParseSourceType {
